f302beac0f28734b5aeb7261a1ca99874b03dfc0
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2018-09-07 11:19:47 +0200

    Added some documentation

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # DCEP2OPUS
 
-DCEP2OPUS converts documents from the DCEP corpus to the OPUS format, to allow easier processing in other applications.
+DCEP2OPUS converts documents from the [DCEP parallel corpus](https://ec.europa.eu/jrc/en/language-technologies/dcep) to the [OPUS](http://opus.nlpl.eu/) format, to allow easier processing in other applications dealing with parallel corpora.
+
+## Installation
+
+DCEP2OPUS requires the following to be installed/downloaded:
+
+* The [DCEP corpus](https://ec.europa.eu/jrc/en/language-technologies/dcep#Download%20the%20DCEP%20corpus).
+* [Uplug](https://bitbucket.org/tiedemann/uplug/wiki/Home). Uplug comes with [hunalign](https://github.com/danielvarga/hunalign) installed.
+* [TreeTagger](http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/). DCEP2OPUS expects TreeTagger to be installed in `/opt/treetagger/`.
 
 ## Details 
 
-The OPUS format is XML-based.
-We use Uplug to tokenize the text, and TreeTagger for part-of-speech tagging.
-# DCEP2OPUS
+The OPUS format is XML-based, while DCEP is text-based.
+Since there is already quite some tooling available for parallel corpora in OPUS, we created a Python script which converts the DCEP corpus into the OPUS format.
+We apply a little bit of preprocessing to cut out the non-plain-text parts of the DCEP corpus. 
+We then use Uplug to tokenize the text, and TreeTagger for part-of-speech tagging.
+We also redo the original sentence alignment using hunalign.
+The resulting corpus can then be queried using, for example, the [PerfectExtractor](https://github.com/UUDigitalHumanitieslab/perfectextractor/) package.
--- process.py
+++ process.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import argparse
 import datetime
 import glob
@@ -12,18 +14,28 @@ from merge_alignments import merge
 from treetagger_xml.xml import process_single
 from treetagger_xml.utils import instantiate_tagger
 
+
 def preprocess(languages, languages_dirs):
-    # Tokenize and convert to XML
+    """
+    Preprocesses all files for a language
+    :param languages: the current languages
+    :param languages_dirs: the current directories per language
+    """
     for language in languages:
         for filename in glob.glob(os.path.join(languages_dirs[language], '*.txt')):
             preprocess_single(language, filename)
 
 
 def preprocess_single(language, filename):
-    # Preprocess:
-    # - Remove lines before (and including) the date of the article.
-    # - Remove lines when the attachments start
-    # - Add paragraph breaks when a potential title is encountered
+    """
+    Preprocess: a single file
+    - Remove lines before (and including) the date of the article.
+    - Remove lines when the attachments start
+    - Add paragraph breaks when a potential title is encountered
+
+    :param language: the current language
+    :param filename: the current filename
+    """
     lines = []
     with open(filename, 'rb') as f:
         look_for_date = True
@@ -63,18 +75,43 @@ def preprocess_single(language, filename):
         f.write('\n'.join(lines))
 
 
-def process(input_dir, output_dir, languages):
+def process(input_dir, output_dir, languages, fetch_limit, input_filter):
+    """
+    Processes the DCEP-corpus and converts it to the proper OPUS format
+    :param input_dir: the input directory (will remain untouched)
+    :param output_dir: the output directory (where all output is stored)
+    :param languages: the languages for which to extract data
+    :param fetch_limit: the maximum number of documents (per document type) to extract
+    :param input_filter: the document types to extract
+    """
+    print('Creating language directories...')
     languages_dirs = create_output_dirs(languages, output_dir)
-    # fetch_raw(input_dir, languages, languages_dirs, 200)
-    # preprocess(languages, languages_dirs)
-    # tokenize(languages, languages_dirs)
+
+    print('Fetching raw .txt-files...')
+    fetch_raw(input_dir, languages, languages_dirs, fetch_limit, input_filter)
+
+    print('Preprocessing .txt-files...')
+    preprocess(languages, languages_dirs)
+
+    print('Tokenizing .txt-files into .xml-files...')
+    tokenize(languages, languages_dirs)
+
+    print('Applying TreeTagger...')
     treetag(languages, languages_dirs)
 
-    # sentence_align(input_dir, output_dir, languages, languages_dirs)
-    # merge_alignments(output_dir, languages)
+    print('Aligning on sentence-level...')
+    sentence_align(input_dir, output_dir, languages, languages_dirs)
+
+    print('Merging the alignments...')
+    merge_alignments(output_dir, languages)
 
 
 def treetag(languages, languages_dirs):
+    """
+    Adds part-of-speech-tags and lemmatization to .xml-files
+    :param languages: the current languages
+    :param languages_dirs: the current directories per language
+    """
     for language in languages:
         tagger = instantiate_tagger(language)
         for src in glob.glob(os.path.join(languages_dirs[language], '*.xml')):
@@ -82,6 +119,11 @@ def treetag(languages, languages_dirs):
 
 
 def merge_alignments(output_dir, languages):
+    """
+    Merges the created alignments into a single file
+    :param output_dir: the output directory (where all output is stored)
+    :param languages: the current languages
+    """
     comb_ls = itertools.combinations(sorted(languages), 2)
     for sl, tl in comb_ls:
         # Merge alignment files
@@ -89,12 +131,19 @@ def merge_alignments(output_dir, languages):
         merged_file = os.path.join(output_dir, '{sl}-{tl}.xml'.format(sl=sl, tl=tl))
         merge(alignments, merged_file)
 
-        # Remove individual files
+        # Remove individual alignment files
         for alignment in alignments:
             os.remove(alignment)
 
 
 def sentence_align(input_dir, output_dir, languages, languages_dirs):
+    """
+    Applies sentence alignment (using hunalign) to the DCEP corpus
+    :param input_dir: the input directory (will remain untouched)
+    :param output_dir: the output directory (where all output is stored)
+    :param languages: the current languages
+    :param languages_dirs: the current directories per language
+    """
     # Look up the translations
     translations = dict()
     comb_ls = itertools.combinations(sorted(languages), 2)
@@ -110,17 +159,18 @@ def sentence_align(input_dir, output_dir, languages, languages_dirs):
         sl = comb_l[0]
         tl = comb_l[1]
         for i, src in enumerate(glob.glob(os.path.join(languages_dirs[sl], '*.xml'))):
+            src_lookup = os.path.splitext(os.path.basename(src))[0]
+
             try:
-                src_lookup = os.path.splitext(os.path.basename(src))[0]
                 trg_lookup = translations[src_lookup]
             except KeyError:
-                print 'File {} not found in the indices file'.format(src_lookup)
+                print('File {} not found in the indices file'.format(src_lookup))
                 continue
 
             src = os.path.join(*(src.split(os.path.sep)[1:]))  # Removes the first folder in the path, see https://stackoverflow.com/a/26724413
             trg = os.path.join(tl, trg_lookup + '.xml')
             if not os.path.exists(os.path.join(output_dir, trg)):
-                print 'Translation for {} ({}) not found'.format(src_lookup, trg)
+                print('Translation for {} ({}) not found'.format(src_lookup, trg))
                 continue
 
             # Create alignment files
@@ -144,33 +194,39 @@ def create_output_dirs(languages, output_dir):
 
 
 def tokenize(languages, languages_dirs):
+    # Tokenize and convert to XML
     for language in languages:
         for filename in glob.glob(os.path.join(languages_dirs[language], '*.prep')):
             f = os.path.splitext(filename)[0]
             command = 'uplug -f pre/basic pre/{language}/basic -in {f}.prep > {f}.xml'.format(language=language, f=f)
-            subprocess.call(command, shell=True)
+            subprocess.call(command, shell=True, stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
 
 
-def fetch_raw(input_dir, languages, languages_dirs, limit=0):
+def fetch_raw(input_dir, languages, languages_dirs, fetch_limit, input_filter):
     # Grab the raw text files
-    in_dir = os.path.join(input_dir, 'sentence/xml')
     for language in languages:
-        source_dir = os.path.join(in_dir, language.upper(), 'IM-PRESS')
+        in_dir = os.path.join(os.path.join(input_dir, 'sentence/xml'), language.upper())
+        source_dirs = os.listdir(in_dir)
+        if input_filter:
+            source_dirs = input_filter
 
-        n = 0
-        for filename in glob.glob(os.path.join(source_dir, '*.txt')):
-            shutil.copy(filename, languages_dirs[language])
-            n += 1
-            if limit and n == limit:
-                break
+        for source_dir in source_dirs:
+            n = 0
+            for filename in glob.glob(os.path.join(in_dir, source_dir, '*.txt')):
+                shutil.copy(filename, languages_dirs[language])
+                n += 1
+                if fetch_limit and n == fetch_limit:
+                    break
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert DCEP files to OPUS format (.xml)')
-    parser.add_argument('dcep_location', type=str, help='DCEP location')
+    parser.add_argument('dcep_location', help='DCEP location')
+    parser.add_argument('output_location', help='Output location')
     parser.add_argument('--languages', nargs='+', help='Languages to select')
-    # parser.add_argument('dcep_filter', type=str, help='Filter on the type of documents to convert')
-    parser.add_argument('output_location', type=str, help='Output location')
+    parser.add_argument('--limit', type=int, default=0, help='Number of documents to select')
+    parser.add_argument('--filter', nargs='*', help='Filter on the type of documents to convert')
+
     args = parser.parse_args()
 
-    process(args.dcep_location, args.output_location, args.languages)
+    process(args.dcep_location, args.output_location, args.languages, args.limit, args.filter)
